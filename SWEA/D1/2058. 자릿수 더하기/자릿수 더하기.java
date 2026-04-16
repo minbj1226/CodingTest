bc28916e8dc2b767b0e07b2004edf3f9bce26962
minbj1226 <154793933+minbj1226@users.noreply.github.com>
2026-04-16 12:44:10 +0900

    [D1] Title: 자릿수 더하기, Time: 72 ms, Memory: 23,296 KB -BaekjoonHub

---
+++ SWEA/D1/2058. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+class Solution
+{
+	public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String N = br.readLine();
+        int num =0;
+        
+        for(int i=0; i<N.length(); i++) {
+            num+=N.charAt(i) - '0';
+        }
+
+        System.out.println(num);
+    }
+}
