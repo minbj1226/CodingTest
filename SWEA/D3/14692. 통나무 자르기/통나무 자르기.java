c49cb5d381309580f41cd99f3aff8cc34edf257a
minbj1226 <154793933+minbj1226@users.noreply.github.com>
2026-05-17 16:43:29 +0900

    [D3] Title: 통나무 자르기, Time: 102 ms, Memory: 27,136 KB -BaekjoonHub

---
+++ SWEA/D3/14692. 통나무 자르기/통나무 자르기.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T=sc.nextInt();
+
+		for(int test_case = 1; test_case <= T; test_case++) {
+			int N = sc.nextInt();
+			String msg = "";
+			
+			if((N-1) % 2 == 1) {
+				msg = "Alice";
+			} else {
+				msg = "Bob";
+			}
+			
+			System.out.println("#" + test_case + " " + msg);
+		}
+	}
+}
