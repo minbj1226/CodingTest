c86d4ea39d705d1d91fca9fe77274490fd45fea0
minbj1226 <154793933+minbj1226@users.noreply.github.com>
2026-07-21 00:13:31 +0900

    [level 2] Title: 피보나치 수, Time: 1.18 ms, Memory: 84.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int n) {
+        int prev = 0; //f(0)
+        int curr = 1; //f(1)
+
+        for(int i=2; i<=n; i++) {
+            int next = (prev + curr) % 1234567;
+
+            prev = curr;
+            curr = next;
+        }
+        
+        return curr;
+    }
+}
